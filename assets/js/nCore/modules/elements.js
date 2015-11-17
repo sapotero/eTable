7e2d974ddcdfad6514abd0e41bc81a40d7ac1749
Sapotero Dominiko <egokok@gmail.com>
2015-11-17 16:12:57 +0300

    Добавление D&D

--- assets/js/nCore/modules/elements.js
+++ assets/js/nCore/modules/elements.js
@@ -44,15 +44,18 @@ nCore.modules.elements = (function(config){
     var fragment = new DocumentFragment();
 
     for (var i = 0; i < e.length; i++) {
+      // для теста создаем элементы в таком виде
+      // <a href="#" class="list-group-item nCore-draggable"> Morbi leo risus </a>
       var a = document.createElement('a');
       a.href = "#";
-      a.className = 'list-group-item';
+      a.className = 'list-group-item draggable';
       a.text = e[i];
       fragment.appendChild(a);
     };
-    nCoreElements.appendChild(fragment);
 
-    // <a href="#" class="list-group-item nCore-draggable"> Morbi leo risus </a>
+    nCoreElements.appendChild(fragment);
+    attachEvent();
+    
     return new nCoreElement();
   },
   create = function(options){
@@ -64,6 +67,52 @@ nCore.modules.elements = (function(config){
   },
   index = function(){
 
+  },
+  attachEvent = function(){
+    interact('.draggable')
+      .draggable({
+        // enable inertial throwing
+        inertia: true,
+        // keep the element within the area of it's parent
+        restrict: {
+          restriction: "parent",
+          endOnly: true,
+          elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+        },
+        // enable autoScroll
+        autoScroll: true,
+
+        // call this function on every dragmove event
+        onmove: dragMoveListener,
+        // call this function on every dragend event
+        onend: function (event) {
+          var textEl = event.target.querySelector('p');
+
+          textEl && (textEl.textContent =
+            'moved a distance of '
+            + (Math.sqrt(event.dx * event.dx +
+                         event.dy * event.dy)|0) + 'px');
+        }
+      });
+
+      function dragMoveListener (event) {
+        var target = event.target,
+            // keep the dragged position in the data-x/data-y attributes
+            x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+            y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+        // translate the element
+        target.style.webkitTransform =
+        target.style.transform =
+          'translate(' + x + 'px, ' + y + 'px)';
+
+        // update the posiion attributes
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
+      }
+
+      // this is used later in the resizing and gesture demos
+      window.dragMoveListener = dragMoveListener;
   }
 
   return {
