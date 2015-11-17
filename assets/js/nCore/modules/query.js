cd6336f26dced1643a8f3e3f932354d0043928c5
Sapotero Dominiko <egokok@gmail.com>
2015-11-17 13:31:58 +0300

    Добавление критериев поиска

--- assets/js/nCore/modules/query.js
+++ assets/js/nCore/modules/query.js
@@ -4,29 +4,56 @@
 var nCore = nCore || {};
 nCore.query = (function(){
 
-  var init = function(){
+  var request, root, body, button,
+  template = ' <form class="form-horizontal fm"> <div class="form-group"> <label for="nCoreTabConfigTextFontSize" class="col-lg-3 control-label">Поле</label> <div class="col-lg-9"> <select class="form-control" id="nCoreTabConfigTextFontSize"> </select> </div></div><div class="form-group" style="padding: 10px auto ;"> <label for="nCoreTabConfigTextFontSize" class="col-lg-3 control-label">Условие</label> <div class="col-lg-9"> <select class="form-control" id="nCoreTabConfigTextFontSize"> </select> </div></div><div class="form-group"> <label class="control-label col-lg-3 " for="focusedInput">Значение</label> <div class="col-lg-9"> <input class="form-control" id="focusedInput" type="text" value=""> </div></div></form>';
+
+  // рисуем форму поиска
+  var init = function(config){
+    root   = document.getElementById( config.nCoreQuery ),
+    button = document.getElementById( config.nCoreQueryAddButtom );
+
+    button.addEventListener('click', function (e) {
+      root.innerHTML += template;
+    });
    },
-  post = function( config ){
-    console.log('query -> post -> ', config);
+
+  // произвольный пост запрос
+  post = function( url ){
+    console.log('query -> post -> ', url);
    },
-  get  = function( config ){
-    console.log('query -> get -> ', config);
+  // произвольный гет запрос
+  get  = function( url ){
+    console.log('query -> get -> ', url);
    },
+
+  // получаем критерии поиска
   getQuery  = function( url ){
     console.log('query -> getQuery -> ', url);
-    
    },
+
+  // получаем параметры для выбранного критерия поиска
   getQueryParam  = function( config ){
     console.log('query -> getQueryParam -> ', config);
+   },
+
+   // клик на добавление норвого критерия поиска
+  createSearchRequestForm = function(data){
+    nCore.query.request.publish('createSearchRequestForm', data);
    };
 
   return {
-    init          : init,
-    post          : post,
-    get           : get,
-    getQuery      : getQuery,
-    getQueryParam : getQueryParam
-  }
+    init                    : init,
+    post                    : post,
+    get                     : get,
+    getQuery                : getQuery,
+    getQueryParam           : getQueryParam,
+    createSearchRequestForm : createSearchRequestForm,
+    request : request
+  };
 })();
-nCore.query.init();
-var data = nCore.query.getQuery('http://localhost:3000/query/index.json');
+
+nCore.query.init({
+  nCoreQuery: 'nCoreQuery',
+  nCoreQueryAddButtom: 'nCoreQueryAddButtom'
+});
+// var data = nCore.query.getQuery('http://localhost:3000/query/index.json');
