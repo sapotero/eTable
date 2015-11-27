7dde9fe3d759d6606a4f10dfc1e162d60c4c793b
Sapotero Dominiko <egokok@gmail.com>
2015-11-27 13:38:41 +0300

    Рефакторинг

--- assets/js/nCore/nCore.js
+++ assets/js/nCore/nCore.js
@@ -1,33 +1,46 @@
 "use strict";
-// наш микро фреймворк nCore
-// 
-var nCore = nCore || {};
 
-nCore = (function(){
-  
-  function loadModules(){
-    nCore.modules = {};
-    nCore.core    = {};
-    nCore.query   = {};
+(function (root, factory) {
+  var define = root.define;
 
-    var modules = ['events', 'elements', 'query', 'cell', 'cellEditor','table', 'document', 'core'];
-    var body = document.getElementsByTagName('body')[0];
-
-    for (var i = modules.length - 1; i >= 0; i--) {
-      var js_script = document.createElement('script');
-      js_script.type = "text/javascript";
-      js_script.src =  'assets/js/nCore/modules/'+modules[i]+'.js';
-      body.appendChild(js_script);
-    };
-  };
-
-  function init(){
-    loadModules();
+  if (define && define.amd) {
+    define([], factory);
+  } else if (typeof module !== 'undefined' && module.exports) {
+    module.exports = factory();
+  } else {
+    root.nCore = factory();
+    root.nCore.modules = {};
+    root.nCore.core    = {};
+    root.nCore.query   = {};
+    root.nCore.router  = {};
   }
+}(this, function () {
+    function loadModules(){
+      var dependencies = {
+        core    : ["core", "router"],
+        modules : ["document", "table", "cellEditor", "cell", "query", "events"]
+      },
+      body = document.getElementsByTagName('body')[0];
+      
+      for (var type in dependencies){
+        if ( dependencies.hasOwnProperty(type) ) {
 
-  return {
-    init: init
-  };
-})();
+          for (var i in dependencies[type]){
+            var js_script = document.createElement('script');
+            js_script.src = 'assets/js/nCore/'+type+'/'+dependencies[type][i]+'.js';
+            body.appendChild(js_script);
+          }
+        };
+      };
+    };
+
+    function init(){
+      loadModules();
+    }
+
+    return {
+      init: init
+    };
+}));
 
 nCore.init();
